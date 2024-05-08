ca033e041f21d464eb617028a8608f35992741aa
Diosk <diosk.dv@gmail.com>
2024-05-08 08:12:26 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games {'gta5'}
 lua54 'yes'
 
 author 'DV Diosk'
-description 'continuously checks and deletes duplicatet vehicles -  https://discord.gg/rCAEajj5x5'
+description 'Überprüft und löscht fortlaufend doppelte Fahrzeuge'
 version '0.0.1'
 
 server_script 'server.lua'
