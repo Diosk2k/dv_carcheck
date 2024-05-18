ab545a592bd3a4db19b3e5a69d9ac9dd180a01d2
Diosk <diosk.dv@gmail.com>
2024-05-18 13:46:56 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,47 +1,45 @@
 AddEventHandler('onResourceStart', function(resourceName)
-	if (GetCurrentResourceName() ~= resourceName) then
-	  return
-	end
-	print('Die Ressource ' .. resourceName .. ' wurde gestartet.')
+    if (GetCurrentResourceName() ~= resourceName) then
+        return
+    end
+    print('Ressource ' .. resourceName .. ' started!')
 end)
-  
-
-local alwaysCheck = true
 
 if alwaysCheck then
-	CreateThread(function()
-		while true do
-			local plateList = {}
-			local allVeh = GetAllVehicles()
-			for i=1, #allVeh do
-				if DoesEntityExist(allVeh[i]) then
-					local plate = GetVehicleNumberPlateText(allVeh[i])
-					local model = GetEntityModel(allVeh[i])
-					if not plateList[plate] then 
-						plateList[plate] = model
-					elseif model == plateList[plate] then
-						DeleteEntity(allVeh[i])
-					end
-				end
-			end
-			Wait(5000)
-		end
-	end)
+    Citizen.CreateThread(function()
+        while true do
+            local plateList = {}
+            local allVeh = GetAllVehicles()
+            for i=1, #allVeh do
+                local vehicle = allVeh[i]
+                if DoesEntityExist(vehicle) then
+                    local plate = GetVehicleNumberPlateText(vehicle)
+                    local model = GetEntityModel(vehicle)
+                    if not plateList[plate] then 
+                        plateList[plate] = model
+                    elseif model == plateList[plate] then
+                        DeleteEntity(vehicle)
+                    end
+                end
+            end
+            Citizen.Wait(2000)
+        end
+    end)
 else
-	AddEventHandler("checkifdouble", function(entity)
-		if DoesEntityExist(entity) and GetEntityType(entity) == 2 then
-			local myVehiclePlate = GetVehicleNumberPlateText(entity)
-			local myVehicleModel = GetEntityModel(entity)
-			local allVeh = GetAllVehicles()
-			for i=1, #allVeh do
-				local veh = allVeh[i]
-				if DoesEntityExist(veh) then
-					if myVehiclePlate == GetVehicleNumberPlateText(veh) and myVehicleModel == GetEntityModel(veh) and entity ~= veh then
-						CancelEvent()
-						break
-					end
-				end
-			end
-		end
-	end)
+    AddEventHandler("checkifdouble", function(entity)
+        if DoesEntityExist(entity) and GetEntityType(entity) == 2 then
+            local myVehiclePlate = GetVehicleNumberPlateText(entity)
+            local myVehicleModel = GetEntityModel(entity)
+            local allVeh = GetAllVehicles()
+            for i=1, #allVeh do
+                local veh = allVeh[i]
+                if DoesEntityExist(veh) then
+                    if myVehiclePlate == GetVehicleNumberPlateText(veh) and myVehicleModel == GetEntityModel(veh) and entity ~= veh then
+                        CancelEvent()
+                        break
+                    end
+                end
+            end
+        end
+    end)
 end
